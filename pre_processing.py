9940fb1d6f16299593b20cb31eea24c99a075c54
Chris Al Gerges <chrisalgerges@hotmail.com>
2018-06-07 23:53:36 +0200

    started cleaning the data

--- pre_processing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("WFPVAM_FoodPrices_05-12-2017.csv", encoding="ISO-8859-1")
-
-df.columns = ["country_id", "country", "region_id", "region", "city_id", "city", "food_id", "food", "network_id", "network", "type_id", "type", "quantity", "unit_measure", "month", "year", "price", "commodity_source"]
-
-print(df)
