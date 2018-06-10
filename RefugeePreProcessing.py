ca963ac1aa16aa338287896b743bd58c74565648
DSalimans <Diederik.Salimans@gmail.com>
2018-06-10 15:56:03 +0200

    cleaned refugee dataset

--- RefugeePreProcessing.py
+++ RefugeePreProcessing.py
@@ -8,30 +8,30 @@ UNRaw = pd.read_csv("UNDataraw.csv")
 def yearlyOrigin(dataset):
     dataset2 = pd.to_numeric(dataset["Total refugees"].groupby([dataset['Origin country'], dataset['Year']]).sum(), downcast='signed')
     dataset2.columns = ["Country", "Year", "Total refugees"]
-    dataset2.to_csv("YearlyRefugeeOutput.csv", header='Country, Year, Total refugees')
+    dataset2.to_csv("YearlyRefugeeOutput.csv", header='Country, Year, Total refugees out')
     return dataset2
 
 # haalt jaarlijkse refugees input per land uit dataset
 def yearlyTarget(dataset):
     dataset2 = pd.to_numeric(dataset["Total refugees"].groupby([dataset['Target country'], dataset['Year']]).sum(), downcast='signed')
-    dataset2.to_csv("YearlyRefugeeInput.csv", header='Country, Year, Total refugees')
+    dataset2.to_csv("YearlyRefugeeInput.csv", header='Country, Year, Total refugees in')
     return dataset2
 
 # merged beide totale refugee csv's
-def merger(Origin, Target): 
-    result = pd.merge(Origin, Target, how='outer', on=('Country', 'Year'))
+def merger(): 
+    result = pd.merge(pd.read_csv('YearlyRefugeeOutput.csv'), pd.read_csv('YearlyRefugeeInput.csv'), how='outer', on=('Country', 'Year'))
     result = result.fillna(0)
     result = result.sort_values('Country')
-    result = pd.to_numeric(result, downcast='signed')
+    result['Total refugees in'] = result['Total refugees in'].apply(np.int64)
+    result['Total refugees out'] = result['Total refugees out'].apply(np.int64)
+    print(result)
     result.to_csv('YearlyRefugees.csv')
 
-def main():
-    print(yearlyOrigin(UNRaw))
-    #yearlyTarget(UNRaw)
-    #merger(yearlyOrigin, yearlyTarget)
 
-main()
 
+#yearlyTarget(UNRaw)
+#yearlyOrigin(UNRaw)
+#merger()
 
 
 
