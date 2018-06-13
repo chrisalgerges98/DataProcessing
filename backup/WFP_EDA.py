d26b70c943d8671f030fc5d3f1d836f6566cb777
DSalimans <Diederik.Salimans@gmail.com>
2018-06-13 19:56:35 +0200

    added OpvallendeRelaties & backed up files

--- backup/WFP_EDA.py
+++ backup/WFP_EDA.py
@@ -1,5 +1,47 @@
 import pandas as pd
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
-df = pd.read_csv("WFPAveragePriceCleaned")
+df = pd.read_csv("WFPCleaned.csv")
 
-print(df["food"].unique())
+countries = df["country"].unique()
+print(countries)
+
+# Afghan_fuel = df["year"][(df["country"] == "Afghanistan") & (df["food"] == "Fuel (diesel)")]
+# Afghan_rice = df["year"][(df["country"] == "Afghanistan") & (df["food"] == "Rice (low quality)")]
+# Afghan_wheat = df["year"][(df["country"] == "Afghanistan") & (df["food"] == "Wheat")]
+#
+# y_1 = df["average_price"][(df["country"] == "Afghanistan") & (df["food"] == "Fuel (diesel)")]
+# y_2 = df["average_price"][(df["country"] == "Afghanistan") & (df["food"] == "Rice (low quality)")]
+# y_3 = df["average_price"][(df["country"] == "Afghanistan") & (df["food"] == "Wheat")]
+#
+# output_file("Afghanistan.html")
+#
+# f = figure(plot_width=300, plot_height=300)
+#
+# f.multi_line(xs = [Afghan_fuel, Afghan_rice, Afghan_wheat], ys = [y_1, y_2, y_3], color=["red", "green", "blue"])
+#
+# show(f)
+
+# f.multi_line(xs = [df["year"][(df["country"] == "Afghanistan") & (df["food"] == "Fuel (diesel)")],
+#                    df["year"][(df["country"] == "Afghanistan") & (df["food"] == "Rice (low quality)")]
+#                    df["year"][(df["country"] == "Afghanistan") & (df["food"] == "Wheat")]])
+
+for country in countries:
+    x_list = []
+    y_list = []
+    products = df["food"][df["country"] == country].unique()
+    for product in products:
+        x = df["year"][(df["country"] == country) & (df["food"] == product)]
+        print(x)
+        y = df["average_price"][(df["country"] == country) & (df["food"] == product)]
+        x_list.append(x)
+        y_list.append(y)
+    fOut = open("chart1.html", "a")
+    f = figure(plot_width=500, plot_height=500, title=country)
+    f.multi_line(xs = x_list, ys = y_list)
+    html = file_html(f, CDN, "chart1")
+    fOut.write(html)
+    fOut.close()
--- backup/count.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("out.csv")
-
-df = df["year"].groupby([df["country"], df["food"]]).count()
-
-df.to_csv("out_2.csv")
