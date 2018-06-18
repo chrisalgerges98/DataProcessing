a60fb0ed044a76a2ad54724ee836a1c957e3bf7b
Dion Custers <dicutoro@gmail.com>
2018-06-18 14:00:43 +0200

    updated wfp_region.py and deleted testregion2.html

--- WFP_Region.py
+++ WFP_Region.py
@@ -7,8 +7,8 @@ from bokeh.embed import file_html
 land = [["Afghanistan"],["Armenia"],["Bangladesh"],["Benin"],["Bhutan"],["Bolivia"],["Burkina Faso"],["Burundi"],["Cambodia"],["Cameroon"],["Central African Republic"],["Chad"],["Colombia"],["Democratic Republic of the Congo"],["Congo Brazzaville"],["Congo Kinshasa"],
 ["Costa Rica"],["Cote d'Ivoire"], ["Djibouti"],["Egypt"],["El Salvador"],["Ethiopia"],["Gambia"],["Georgia"],["Ghana"],["Guatemala"],["Guinea"],["Guinea-Bissau"],["Haiti"],["Honduras"],["India"],["Indonesia"],["Iran (Islamic Republic of)"],
 ["Iraq"],["Jordan"],["Kenya"],["Kyrgyzstan"],["Lao people's Democratic Republic"],["Lebanon"],["Lesotho"],["Liberia"],["Madagascar"],["Malawi"],["Mali"],["Mauritania"],["Mozambique"],["Myanmar"],["Nepal"],["Niger"],["Nigeria"],
-["Pakistan"],["Panama"],["Peru"],["Philippines"],["Rwanda"],["Senegal"],["Somalia"],["South Sudan"],["Sri Lanka"],["Sudan"],["Swaziland"],["Syrian Arab Republic"],["Tajikistan"],['Uganda'],['United Republic of Tanzania'],
-['Yemen'],['Zambia']]
+["Pakistan"],["Panama"],["Peru"],["Philippines"],["Rwanda"],["Senegal"],["Somalia"],["South Sudan"],["Sri Lanka"],["Sudan"],["Swaziland"],["Tajikistan"],['Uganda'],['United Republic of Tanzania'],
+['Yemen'],['Zambia'],['Zimbabwe']]
 
 regio = [["Afghanistan","Iran (Islamic Republic of)", "Pakistan", "Tajikistan"],["Amernia" "Iran (Islamic Republic of)", "Georgia"],["Bangladesh" "India", "Myanmar"],["Benin","Burkina Faso", "Nigeria", "Niger"],["Benin","Burkina Faso", "Nigeria", "Niger"],
 ["Bhutan","India"],["Bolivia", "Peru"],["Burkina Faso","Mali", "Cote d'Ivoire", "Ghana", "Benin", "Niger"],["Burundi","Congo-Kinshasa", "Rwanda", "United Republic of Tanzania"],["Cambodia","Lao people's Democratic Republic"],
@@ -34,19 +34,19 @@ regio = [["Afghanistan","Iran (Islamic Republic of)", "Pakistan", "Tajikistan"],
 ["India","Pakistan", "Nepal", "Bhutan", "Bangladesh", "Myanmar"],
 ["Indonesia","Philippines"],
 ["Iran (Islamic Republic of)","Afghanistan", "Pakistan", "Iraq", "Armenia"],
-["Iraq","Syrian Arab Republic", "Iran (Islamic Republic of)", "Jordan"],
-["Jordan","Iraq", "Syrian Arab Republic"],
+["Iraq", "Iran (Islamic Republic of)", "Jordan"],
+["Jordan","Iraq"],
 ["Kenya","Somalia", "Ethiopia", "Uganda", "United Republic of Tanzania"],
 ["Kyrgyzstan","Tajikistan"],
 ["Lao people's Democratic Republic","Cambodia", "Myanmar"],
-["Lebanon","Syrian Arab Republic"],
+["Lebanon"],
 ["Lesotho"],
 ["Liberia","Guinea", "Cote d'Ivoire"],
 ["Madagascar"],
 ["Malawi","Mozambique", "Zambia", "United Republic of Tanzania"],
 ["Mali","Niger", "Burkina Faso", "Cote d'Ivoire", "Mauritania", "Senegal", "Guinea"],
 ["Mauritania","Mali", "Senegal"],
-["Mozambique","Malawi", "United Republic of Tanzania", "Zambia", "Swaziland"],
+["Mozambique","Malawi", "United Republic of Tanzania", "Zambia", "Swaziland", "Zimbabwe"],
 ["Myanmar","Lao people's Democratic Republic", "Bangladesh", "India"],
 ["Nepal","India"],
 ["Niger","Mali", "Chad", "Nigeria", "Benin", "Burkina Faso"],
@@ -62,12 +62,12 @@ regio = [["Afghanistan","Iran (Islamic Republic of)", "Pakistan", "Tajikistan"],
 ["Sri Lanka"],
 ["Sudan","Chad", "Central African Republic", "Egypt", "Ethiopia"],
 ["Swaziland","Mozambique"],
-["Syrian Arab Republic","Lebanon", "Jordan"],
 ["Tajikistan","Kyrgyzstan", "Afghanistan"],
 ["Uganda","Kenya", "South Sudan", "Democratic Republic of the Congo", "Rwanda", "United Republic of Tanzania"],
 ["United Republic of Tanzania","Kenya", "Uganda", "Rwanda", "Burundi", "Democratic Republic of the Congo", "Zambia", "Malawi"],
 ["Yemen","Djibouti", "Somalia"],
-["Zambia","United Republic of Tanzania", "Malawi", "Mozambique"]]
+["Zambia","United Republic of Tanzania", "Malawi", "Mozambique", "Zimbabwe"],
+["Zimbabwe", "Zambia", "Mozambique"]]
 
 
 #Print till Cote d'Ivoiry but not further don't know why Probably something WFP_csv.
@@ -92,16 +92,19 @@ product = "Maize"
 for i, country in zip(range(len(land)), countries):
     x_list = []
     y_list = []
-    #print(land[i])
-    for j in regio[i]:
-        x = df["year"][(df["country"] == j) & (df["food"] == product)]
-        #print(x)
-        y = df["average_price"][(df["country"] == j) & (df["food"] == product)]
-        x_list.append(x)
-        y_list.append(y)
-    fOut = open("testregion2.html", "a")
-    f = figure(plot_width=500, plot_height=500, title=country)
-    f.multi_line(xs = x_list, ys = y_list)
-    html = file_html(f, CDN, "chart2")
-    fOut.write(html)
-    fOut.close()
+    if product in df["food"][(df["country"] == country)].unique():
+        #print(land[i])
+        for j in regio[i]:
+            x = df["year"][(df["country"] == j) & (df["food"] == product)]
+            #print(x)
+            y = df["average_price"][(df["country"] == j) & (df["food"] == product)]
+            x_list.append(x)
+            y_list.append(y)
+        fOut = open("testregion2.html", "a")
+        f = figure(plot_width=500, plot_height=500, title=country)
+        f.multi_line(xs = x_list, ys = y_list)
+        html = file_html(f, CDN, "chart2")
+        fOut.write(html)
+        fOut.close()
+    else:
+        continue
