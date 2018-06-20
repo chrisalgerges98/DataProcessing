1b53bd218050daa62d17013092a493a629f8d226
Dion Custers <dicutoro@gmail.com>
2018-06-20 12:50:20 +0200

    updated wfp_region.py

--- WFP_Region.py
+++ WFP_Region.py
@@ -12,7 +12,7 @@ land =[["Benin"],
 ["Chad"],
 ["Congo"],
 ["Democratic Republic of the Congo"],
-["Cote d'Ivoire"],
+["Ivory Coast"],
 ["Djibouti"],
 ["Egypt"],
 ["Ethiopia"],
@@ -35,36 +35,34 @@ land =[["Benin"],
 ["South Sudan"],
 ["Sudan"],
 ["Swaziland"],
-['Uganda'],
-['United Republic of Tanzania'],
-['Yemen'],
-['Zambia'],
-['Zimbabwe'],
-['MijnlandAfrica']]
+["Uganda"],
+["United Republic of Tanzania"],
+["Zambia"],
+["Zimbabwe"],
+["MijnlandAfrica"]]
 
 regio =[["Benin","Burkina Faso", "Nigeria", "Niger"],
-["Burkina Faso","Mali", "Cote d'Ivoire", "Ghana", "Benin", "Niger"],
+["Burkina Faso","Mali", "Ivory Coast", "Ghana", "Benin", "Niger"],
 ["Burundi","Democratic Republic of the Congo", "Rwanda", "United Republic of Tanzania"],
 ["Cameroon","Nigeria", "Chad", "Central African Republic", "Congo"],
 ["Central African Republic", "Congo", "Democratic Republic of the Congo", "Chad", "Sudan", "South Sudan"],
 ["Chad","Niger", "Nigeria", "Sudan", "Central African Republic", "Cameroon"],
 ["Congo","Democratic Republic of the Congo", "Cameroon", "Central African Republic"],
 ["Democratic Republic of the Congo","Congo", "Zambia", "Rwanda", "Burundi", "United Republic of Tanzania", "Uganda", "South Sudan", "Central African Republic"],
-["Cote d'Ivoire", "Guinea", "Liberia", "Ghana", "Mali", "Burkina Faso"],
+["Ivory Coast", "Guinea", "Liberia", "Ghana", "Mali", "Burkina Faso"],
 ["Djibouti","Ethiopia","Yemen"],
 ["Egypt","Sudan"],
-["El Salvador","Guatemala", "Honduras"],
 ["Ethiopia","Sudan", "Djibouti", "South Sudan", "Kenya"],
 ["Gambia","Senegal"],
-["Ghana","Cote d'Ivoire", "Burkina Faso"],
-["Guinea","Cote d'Ivoire", "Liberia", "Guinea-Bissau", "Senegal", "Mali"],
+["Ghana","Ivory Coast", "Burkina Faso"],
+["Guinea","Ivory Coast", "Liberia", "Guinea-Bissau", "Senegal", "Mali"],
 ["Guinea-Bissau","Guinea", "Senegal"],
 ["Kenya", "Ethiopia", "Uganda", "United Republic of Tanzania"],
 ["Lesotho"],
-["Liberia","Guinea", "Cote d'Ivoire"],
+["Liberia","Guinea", "Ivory Coast"],
 ["Madagascar"],
 ["Malawi","Mozambique", "Zambia", "United Republic of Tanzania"],
-["Mali","Niger", "Burkina Faso", "Cote d'Ivoire", "Mauritania", "Senegal", "Guinea"],
+["Mali","Niger", "Burkina Faso", "Ivory Coast", "Mauritania", "Senegal", "Guinea"],
 ["Mauritania","Mali", "Senegal"],
 ["Mozambique","Malawi", "United Republic of Tanzania", "Zambia", "Swaziland", "Zimbabwe"],
 ["Niger","Mali", "Chad", "Nigeria", "Benin", "Burkina Faso"],
@@ -76,31 +74,32 @@ regio =[["Benin","Burkina Faso", "Nigeria", "Niger"],
 ["Swaziland","Mozambique"],
 ["Uganda","Kenya", "South Sudan", "Democratic Republic of the Congo", "Rwanda", "United Republic of Tanzania"],
 ["United Republic of Tanzania","Kenya", "Uganda", "Rwanda", "Burundi", "Democratic Republic of the Congo", "Zambia", "Malawi"],
-["Yemen","Djibouti"],
 ["Zambia","United Republic of Tanzania", "Malawi", "Mozambique", "Zimbabwe"],
 ["Zimbabwe", "Zambia", "Mozambique"],
-["MijnlandAfrica","Benin","Burkina Faso","Burundi","Cameroon","Central African Republic","Chad","Congo","Democratic Republic of the Congo","Cote d'Ivoire","Djibouti","Egypt","Ethiopia","Gambia","Ghana","Guinea","Guinea-Bissau","Kenya","Lesotho","Liberia","Madagascar","Malawi","Mali","Mauritania","Mozambique","Niger","Nigeria","Rwanda","Senegal","South Sudan","Sudan","Swaziland",'Uganda','United Republic of Tanzania','Yemen','Zambia','Zimbabwe',]]
+["MijnlandAfrica","Benin","Burkina Faso","Burundi","Cameroon","Central African Republic","Chad","Congo","Democratic Republic of the Congo","Ivory Coast","Djibouti","Egypt","Ethiopia","Gambia","Ghana","Guinea","Guinea-Bissau","Kenya","Lesotho","Liberia","Madagascar","Malawi","Mali","Mauritania","Mozambique","Niger","Nigeria","Rwanda","Senegal","South Sudan","Sudan","Swaziland",'Uganda','United Republic of Tanzania','Yemen','Zambia','Zimbabwe',]]
 
 
 
-df = pd.read_csv("WFPFinal.csv")
+df = pd.read_csv("WFPAfricaFinal.csv")
 
 countries = df["country"].unique()
 
 # change to get charts about regions and certain product
-product = "Maize"
+product = "Yam"
 for i, country in zip(range(len(land)), countries):
     x_list = []
     y_list = []
     if product in df["food"][(df["country"] == country)].unique():
-        #print(land[i])
+        print(land[i])
+        print(i)
         for j in regio[i]:
+            print(j)
             x = df["year"][(df["country"] == j) & (df["food"] == product)]
-            #print(x)
+            print(x)
             y = df["price_per_unit"][(df["country"] == j) & (df["food"] == product)]
             x_list.append(x)
             y_list.append(y)
-        fOut = open("Afrika_Maize.html", "a")
+        fOut = open("Afrika_Yam.html", "a")
         f = figure(plot_width=500, plot_height=500, title=country)
         f.multi_line(xs = x_list, ys = y_list)
         html = file_html(f, CDN, "chart")
