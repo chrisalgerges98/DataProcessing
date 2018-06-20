d8dc734667f534b07cb94e556a14b49fb51a51ba
DSalimans <Diederik.Salimans@gmail.com>
2018-06-20 13:51:54 +0200

    merged refugees and RoC

--- ffchecken.py
+++ ffchecken.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import numpy as np 
 
-df_1 = pd.read_csv("ffcheckendataset.csv")
-df_2 = pd.read_csv("WFPCleaned.csv")
+# df_1 = pd.read_csv("ffcheckendataset.csv")
+# df_2 = pd.read_csv("WFPCleaned.csv")
 
-result = pd.merge(df_1, df_2,  how='left', left_on=['country','year','food'], right_on = ['country','year','food'])
+# result = pd.merge(df_1, df_2,  how='left', left_on=['country','year','food'], right_on = ['country','year','food'])
 
-result.to_csv("ffdubbelcheck.csv")
+# result.to_csv("WFPxRefugee.csv")
+
+df_1 = pd.read_csv("AverageRateOfChange.csv")
+df_2 = pd.read_csv("YearlyRefugees.csv")
+
+result = pd.merge(df_1, df_2,  how='left', left_on=['Country','Year'], right_on = ['Country','Year'])
+
+result.to_csv("RateOfChangexRefugees.csv")
