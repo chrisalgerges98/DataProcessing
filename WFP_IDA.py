67f3f8360a7ac5718c75fc30cda5dc3159827ec2
Chris Al Gerges <chrisalgerges@hotmail.com>
2018-06-26 00:12:30 +0200

    created scatterplots of the refugees in percentage of the population and the rate of change of the average price

--- WFP_IDA.py
+++ WFP_IDA.py
@@ -61,13 +61,11 @@ my_palette = ['goldenrod', 'forestgreen', 'black', 'blue', 'blueviolet', 'brown'
 #     fOut.write(html)
 # fOut.close()
 
-with open('my_csv.csv', 'a') as f:
-    for country in countries:
-        for food in products:
-            df_2 = df[(df["country"] == country) & (df["food"] == food)]
-            df_2.to_csv(f)
-
-
+# with open('my_csv.csv', 'a') as f:
+#     for country in countries:
+#         for food in products:
+#             df_2 = df[(df["country"] == country) & (df["food"] == food)]
+#             df_2.to_csv(f)
 
 # def chunks(l, n):
 #     new_list = []
@@ -76,13 +74,12 @@ with open('my_csv.csv', 'a') as f:
 #         new_list.append(elem)
 #     return new_list
 #
-# def price_per_year(country, foodtype):
+# def price_per_year(country):
 #     prices = []
 #     for year in years:
-#         for food in foodtype:
-#             df_1 = df["price_per_unit"][(df["food"] == food) & (df["year"] == year) & (df["country"] == country)]
-#             prices.append(df_1)
-#     new_list = chunks(prices, len(foodtype))
+#         df_1 = df["price_per_unit"][(df["year"] == year) & (df["country"] == country)]
+#         prices.append(df_1)
+#     new_list = chunks(prices, len(products))
 #
 #     new_list_2 = []
 #     for i in new_list:
@@ -97,7 +94,52 @@ with open('my_csv.csv', 'a') as f:
 #         average_price = sum(k) / len(k)
 #         price_per_year.append(average_price)
 #     return price_per_year
-#
+
+
+df_3 = pd.read_csv("AverageCountry.csv")
+
+fOut = open("BeninAverageChart.html", "a")
+f = figure(plot_width=750, plot_height=350, title="average price per country of Maize, Sorghum and Millet")
+legend_it = []
+for country, color in zip(countries, my_palette):
+    source = ColumnDataSource(name = "data", data=dict(
+        average_price = df_3["price"][df_3["country"] == country],
+        year= df_3["year"][df_3["country"] == country]
+    ))
+    hover = HoverTool(tooltips=[
+        ("year", "@year"),
+        ("average_price", "@average_price")
+    ], mode="vline")
+    c = f.line(x="year", y="average_price", line_width=2, color=color, legend=source.name, alpha=0.8, muted_color=color, muted_alpha=0.1, source=source)
+    legend_it.append((country, [c]))
+f.toolbar.tools = [PanTool(), ResetTool(), WheelZoomTool(), hover, LassoSelectTool(), BoxSelectTool()]
+f.xaxis.axis_label = "year"
+f.yaxis.axis_label = "average_price"
+legend = Legend(items=legend_it, location=(0,-10))
+legend.click_policy="hide"
+f.legend.visible=False
+f.add_layout(legend, "right")
+html = file_html(f, CDN, "BeninAverageChart")
+fOut.write(html)
+fOut.close()
+
+
+
+
+# fOut = open("ChartSpecificAveragedChart.html", "a")
+# f = figure(plot_width=550, plot_height=350, title="average price per country of Maize, Sorghum and Millet")
+# for country in countries:
+#     for food, color in zip(products, my_palette):
+#         f.line(df_5["year"][(df_5["country"] == country) & (df_5["food"] == food)], df_5["price_per_unit"][(df["country"] == country) & (df_5["food"] == food)],
+#         color=color, alpha=0.8, legend=country, muted_color=color, muted_alpha=0.1)
+# f.xaxis.axis_label = "year"
+# f.yaxis.axis_label = "average_price"
+# f.legend.click_policy="hide"
+# html = file_html(f, CDN, "ChartSpecificAveragedChart")
+# fOut.write(html)
+# fOut.close()
+
+
 # fOut = open("FoodTypeChart.html", "a")
 # for foodtype, title in zip(Alle_producten, Titles):
 #     legend_it = []
